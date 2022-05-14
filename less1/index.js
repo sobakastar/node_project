dffe85a3ede2e4d6bcca105a1196ac3ee07124c6
sobakastar <dr.gris@yandex.ru>
2022-05-14 20:18:29 +0300

    first commit

---
+++ less1/index.js
@@ -0,0 +1,12 @@
+// const colors = require('colors');
+import colors from "colors";
+// const colors = require("colors/safe");
+
+// console.log(process.argv);
+// const [ serg, bob ] = process.argv.slice(2);
+// console.log('Hello Node.js to ' + serg + ' and ' + bob);
+
+// console.log(process); глобальный объект, описывает текущий процесс
+
+
+
